3c473a87bd0a23e19142414a9d23d5ea0b022946
Carter Warren <cwarren2023@chadwickschool.org>
2022-10-19 20:42:43 -0400

    docs: Recreated deleted branch

---
+++ Shared/Components/Video.cs
@@ -0,0 +1,6 @@
+public class Video {
+    public string Src {get; set; } = "/videos/sample.mp4";
+    public string PreviewImg {get; set; } = "/imgs/Repl.jpg";
+    public string Title {get; set; } = "Title";
+    public string SubTitle {get; set; } = "SubTitle";
+}
