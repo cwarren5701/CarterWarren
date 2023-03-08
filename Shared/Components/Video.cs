07bb6d313187daea3e0f9bb4c095971526794ac0
Carter Warren <cwarren2023@chadwickschool.org>
2023-03-07 17:57:05 -0800

    feat: Implement Video Class for VideoDisplay

--- Shared/Components/Video.cs
+++ Shared/Components/Video.cs
@@ -4,4 +4,16 @@ public class Video {
     public string Title {get; set; } = "Title";
     public string SubTitle {get; set; } = "SubTitle";
     public string Href {get; set; } = "/";
+
+    public Video() {
+        
+    }
+
+    public Video(string s, string p, string t, string sub, string h) {
+        Src = s;
+        PreviewImg = p;
+        Title = t;
+        SubTitle = sub;
+        Href = h;
+    }
 }
--- Shared/MockDatabase.cs
+++ Shared/MockDatabase.cs
@@ -1,7 +1,9 @@
 public class MockDatabase {
     public Video SophomoreChadwick {get; set;} = new Video("/videos/Sophomore-Chadwick-Highlights-2021.mp4", "/imgs/SophomoreChadwick.png", "Sophomore Year Highlights", "Chadwick School (2020-2021)", "/SophomoreChadwick");
     public Video SophomoreWCE {get; set; } = new Video("/videos/West Coast Elite Sophomore Full AAU highlights(2021) .mp4", "/imgs/SophomoreWCE.png", "Sophomore Year Highlights (AAU)", "West Coast Elite (2020-2021)", "/SophomoreWCE");
-    public List<Video> Videos {get; set; } = ();
-    Videos.Add(SophomoreChadwick);
-    Videos.Add(SophomoreWCE);
+    public static List<Video> Videos {get; set; } = new();
+    static MockDatabase() {
+        Videos.Add(new Video("/videos/Sophomore-Chadwick-Highlights-2021.mp4", "/imgs/SophomoreChadwick.png", "Sophomore Year Highlights", "Chadwick School (2020-2021)", "/SophomoreChadwick"));
+        Videos.Add(new Video("/videos/West Coast Elite Sophomore Full AAU highlights(2021) .mp4", "/imgs/SophomoreWCE.png", "Sophomore Year Highlights (AAU)", "West Coast Elite (2020-2021)", "/SophomoreWCE"));
+    }
 }
